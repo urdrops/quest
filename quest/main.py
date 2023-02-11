d385cd651fe61a3078d6e5949462710700f10a71
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-02-11 13:03:54 +0500

    git init

--- quest/main.py
+++ quest/main.py
@@ -1,10 +1,33 @@
+import random
+import time
+
 import pygame
 
+WIDTH = 512
+HEIGHT = 512
+FPS = 165
+
+def random_color():
+    return random.randint(0, 255)
+
 pygame.init()
-screen = pygame.display.set_mode((512, 512))
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Glitch-in-the-Head')
 run = True
+clock = pygame.time.Clock()
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 while run:
+    random_color = (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
+    screen.fill(random_color)
+    clock.tick(FPS)
+    print(clock.get_fps())
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
     pygame.display.update()
+    pygame.display.flip()
