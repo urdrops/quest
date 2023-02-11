d8f4d210b8bad90e54cadf1cb276382fd91e2bbe
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-02-11 14:16:11 +0500

    git init

--- quest/main.py
+++ quest/main.py
@@ -9,21 +9,14 @@ pygame.display.set_icon(img)
 pygame.display.set_caption('Glitc-in-the-Head')  # GITH
 run = True
 square = pygame.Surface((50, 50))
-square.fill('Blue')
-screen.blit(square, (W / 2-25, H / 2-25))
-pygame.display.update()
+# square.fill('Blue')
+# screen.blit(square, (W / 2-25, H / 2-25))
+# pygame.display.update()
 
 
 while run:
-
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-        up = 0
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                print('da')
-                up += 1
-                screen.blit(square, (W / 2-25, up))
-                pygame.display.update()
+    pygame.draw.circle(screen,"RED",(W/2, H/2),100)
+    pygame.display.update()
